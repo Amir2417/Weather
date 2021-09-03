0304e8e798ec1f8707997a7e806c72e6cd173183
Amir2417 <amirsohelto@gmail.com>
2021-09-03 23:46:36 +0600

    input field clear

--- js/weather.js
+++ js/weather.js
@@ -3,12 +3,14 @@ const Api_Keys = `ca197ed6712b1d256ef3c5d319382343`;
 
 const searchByName = () =>{
 
-    const inputText = document.getElementById('inputField').value;
-    
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputText}&appid=${Api_Keys}&units=metric`;
+    const inputText = document.getElementById('inputField');
+    const inputValue = inputText.value;
+    inputText.value = '';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${Api_Keys}&units=metric`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayTheResult(data));
+    
     
 }
 
@@ -17,7 +19,7 @@ const setTheResult = (id,text) => {
 }
 
 const displayTheResult = Temparature => {
-   
+    
     setTheResult('cityName',Temparature.name);
     setTheResult('temperature', Temparature.main.temp);
     setTheResult('condition', Temparature.weather[0].main);
@@ -25,7 +27,9 @@ const displayTheResult = Temparature => {
     const imgSrc = `https://openweathermap.org/img/wn/${Temparature.weather[0].icon}.png`;
     const ImgIcon = document.getElementById('weather-icon');
     ImgIcon.setAttribute('src',imgSrc);
-    
+    const block = document.getElementById('block');
+    block.style.display ="block";
+
 
    
 }
